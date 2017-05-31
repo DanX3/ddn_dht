7193bcdeb88357b1020b928165b3c57b3ef03afd
Daniele Tolomelli <daniel.tolomelli@gmail.com>
2017-05-31 13:43:36 +0200

    refactored for building

--- analyticalModel/main.cpp
+++ analyticalModel/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include "model.h"
+#include "stdlib.h"
+
+void simulateTWrite(double indexStart, double indexStop, double step) {
+    Connection ClientToHome(10., 1e3, 1e-3);
+    Connection HomeToSecondary(0.5, 1e3, 1e-3);
+    std::ofstream modelData;
+    modelData.open("model.dat");
+    for (double dataSize=indexStart; dataSize<indexStop; dataSize*=step) {
+        double totalTime = 0.0;
+        double metadataSize = dataSize * 0.03;
+        totalTime += ClientToHome.networkTime(dataSize + metadataSize);
+        totalTime += HomeToSecondary.networkTime(metadataSize);
+        totalTime += HomeToSecondary.ackTime();
+        totalTime += 2 * 1e-2; //log time
+        totalTime += ClientToHome.ackTime();
+        modelData
+            << dataSize << " "
+            << totalTime << " "
+            << totalTime-dataSize/1e3 << '\n';
+    }
+    modelData.close();
+}
+
+void printUsage(char** argv) {
+    std::cout << "Usage: " << argv[0] << " n" << '\n'
+        << '\t' << "0: T_write" << '\n';
+}
 
 #if PLOT
 int main(int argc, char** argv) {
@@ -13,77 +40,20 @@ int main(int argc, char** argv) {
 
     return 0;
 }
-#endif
-#if TWRITE
+#else
 int main(int argc, char** argv) {
-    Connection ClientToHome(10., 1e3, 1e-3);
-    Connection HomeToSecondary(0.5, 1e3, 1e-3);
-    std::ofstream modelData;
-    modelData.open("model.dat");
-    for (double dataSize=1e-3; dataSize<1e3; dataSize*=1.1) {
-        double totalTime = 0.0;
-        double metadataSize = dataSize * 0.03;
-        totalTime += ClientToHome.networkTime(dataSize + metadataSize);
-        totalTime += HomeToSecondary.networkTime(metadataSize);
-        totalTime += HomeToSecondary.ackTime();
-        totalTime += 2 * 1e-2; //log time
-        totalTime += ClientToHome.ackTime();
-        modelData
-            << dataSize << " "
-            << totalTime << " "
-            << totalTime-dataSize/1e3 << '\n';
+    if (argc == 1) {
+        printUsage(argv);
+        return 1;
     }
-    modelData.close();
-
-
-    double totalTime = 0.0;
-    double dataSize = 1e-3;
-    double metadataSize = dataSize * 0.03;
-    for (int i=0; i<100; i++) {
-        totalTime += ClientToHome.networkTime(dataSize + metadataSize);
-        totalTime += HomeToSecondary.networkTime(metadataSize);
-        totalTime += HomeToSecondary.ackTime();
-        totalTime += 2 * 1e-2; //log time
-        totalTime += ClientToHome.ackTime();
+    int indexChosen = atoi(argv[1]);
+    if (indexChosen == 0) {
+        simulateTWrite(1e-3, 1e3, 1.1);
     }
-    std::cout << "100x small data" << totalTime << '\n';
-    return 0;
-}
-#endif
-
-#if TWRITE
-int main(int argc, char** argv) {
-    Connection ClientToHome(10., 1e3, 1e-3);
-    Connection HomeToSecondary(0.5, 1e3, 1e-3);
-    std::ofstream modelData;
-    modelData.open("model.dat");
-    for (double dataSize=1e-3; dataSize<1e3; dataSize*=1.1) {
-        double totalTime = 0.0;
-        double metadataSize = dataSize * 0.03;
-        totalTime += ClientToHome.networkTime(dataSize + metadataSize);
-        totalTime += HomeToSecondary.networkTime(metadataSize);
-        totalTime += HomeToSecondary.ackTime();
-        totalTime += 2 * 1e-2; //log time
-        totalTime += ClientToHome.ackTime();
-        modelData
-            << dataSize << " "
-            << totalTime << " "
-            << totalTime-dataSize/1e3 << '\n';
+    else {
+        printUsage(argv);
+        return 1;
     }
-    modelData.close();
-
-
-    double totalTime = 0.0;
-    double dataSize = 1e-3;
-    double metadataSize = dataSize * 0.03;
-    for (int i=0; i<100; i++) {
-        totalTime += ClientToHome.networkTime(dataSize + metadataSize);
-        totalTime += HomeToSecondary.networkTime(metadataSize);
-        totalTime += HomeToSecondary.ackTime();
-        totalTime += 2 * 1e-2; //log time
-        totalTime += ClientToHome.ackTime();
-    }
-    std::cout << "100x small data" << totalTime << '\n';
     return 0;
 }
 #endif
