0cd027f10045b24ad2d2e54a9eee0b6871ad67ce
Daniele Tolomelli <daniel.tolomelli@gmail.com>
2017-05-31 11:04:54 +0200

    refactored directory

--- analyticalModel/main.cpp
+++ analyticalModel/main.cpp
@@ -13,7 +13,45 @@ int main(int argc, char** argv) {
 
     return 0;
 }
-#else
+#endif
+#if TWRITE
+int main(int argc, char** argv) {
+    Connection ClientToHome(10., 1e3, 1e-3);
+    Connection HomeToSecondary(0.5, 1e3, 1e-3);
+    std::ofstream modelData;
+    modelData.open("model.dat");
+    for (double dataSize=1e-3; dataSize<1e3; dataSize*=1.1) {
+        double totalTime = 0.0;
+        double metadataSize = dataSize * 0.03;
+        totalTime += ClientToHome.networkTime(dataSize + metadataSize);
+        totalTime += HomeToSecondary.networkTime(metadataSize);
+        totalTime += HomeToSecondary.ackTime();
+        totalTime += 2 * 1e-2; //log time
+        totalTime += ClientToHome.ackTime();
+        modelData
+            << dataSize << " "
+            << totalTime << " "
+            << totalTime-dataSize/1e3 << '\n';
+    }
+    modelData.close();
+
+
+    double totalTime = 0.0;
+    double dataSize = 1e-3;
+    double metadataSize = dataSize * 0.03;
+    for (int i=0; i<100; i++) {
+        totalTime += ClientToHome.networkTime(dataSize + metadataSize);
+        totalTime += HomeToSecondary.networkTime(metadataSize);
+        totalTime += HomeToSecondary.ackTime();
+        totalTime += 2 * 1e-2; //log time
+        totalTime += ClientToHome.ackTime();
+    }
+    std::cout << "100x small data" << totalTime << '\n';
+    return 0;
+}
+#endif
+
+#if TWRITE
 int main(int argc, char** argv) {
     Connection ClientToHome(10., 1e3, 1e-3);
     Connection HomeToSecondary(0.5, 1e3, 1e-3);
